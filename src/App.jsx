00407ec41f38be70f2af0319cdb20ede68f17cc9
Stu <stumac1000@yahoo.com>
2024-10-29 13:24:26 -0700

    Work on "new goal" Form
    Add prop to update state on App.jsx component
    Add a navigation
    etc...

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Form from "./components/Form";
+import { useState } from "react";
 // import styles from "./App.module.css";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+  console.log(goals);
+
+  const handleAddGoal = (newGoal) => {
+    setGoals([...goals, newGoal]);
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/new-goal" element={<Form />} />
+        <Route path="/new-goal" element={<Form onAddGoal={handleAddGoal} />} />
       </Routes>
     </Router>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,71 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Form() {
-  const [formData, setFormData] = useState({ goalName: "" });
+export default function Form({ onAddGoal }) {
+  const [formData, setFormData] = useState({});
   console.log(formData);
 
-  const handleInputChange = (event) =>
-    setFormData({ goalName: event.target.value });
+  const navigate = useNavigate();
+
+  const handleInputChange = (event, inputField) => {
+    setFormData({ ...formData, [inputField]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddGoal(formData);
+    setFormData({});
+    navigate("/");
+  };
+
+  const today = new Date().toISOString().slice(0, 10);
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="goal-name">Goal Name: </label>
       <input
         type="text"
         id="goal-name"
         name="goal-name"
-        value={formData.goalName}
-        onChange={handleInputChange}
+        value={formData.goalName || ""}
+        onChange={(e) => handleInputChange(e, "goalName")}
+        required
+      />
+      <br />
+
+      <label htmlFor="money-amount">Money Amount: $</label>
+      <input
+        type="number"
+        min="0"
+        id="money-amount"
+        name="money-amount"
+        value={formData.moneyAmount || ""}
+        onChange={(e) => handleInputChange(e, "moneyAmount")}
+        required
+      />
+      <br />
+
+      <label htmlFor="due-date">Due Date: </label>
+      <input
+        type="date"
+        min={today}
+        id="due-date"
+        name="due-date"
+        value={formData.dueDate || ""}
+        onChange={(e) => handleInputChange(e, "dueDate")}
+        required
       />
       <div>
-        <button>
+        <button type="button">
           <Link to="/">cancel</Link>
         </button>
-        <button>Add Goal</button>
+        <button type="submit">Add Goal</button>
       </div>
     </form>
   );
 }
+
+Form.propTypes = {
+  onAddGoal: PropTypes.func,
+};
